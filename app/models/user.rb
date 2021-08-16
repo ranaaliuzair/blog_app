d98b5536edb0e8a6fd070210b0e093e6418adbe9
Ali Uzair <rana.alieuzair@gmail.com>
2021-08-17 01:56:17 +0500

    adding user index

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-
+  has_secure_password
   has_many :articles
 
  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 root 'pages#index'
+resources :users, except: [:new]
 
 resources :articles
 get 'about', to: 'pages#about'
+get 'signup', to: 'users#new'
 end
