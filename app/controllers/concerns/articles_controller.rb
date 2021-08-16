e393bc8cd4b6809e1dfccc11701b6efd78010ba9
Ali Uzair <rana.alieuzair@gmail.com>
2021-08-17 00:07:55 +0500

    add association between user and articles

--- app/controllers/concerns/articles_controller.rb
+++ app/controllers/concerns/articles_controller.rb
@@ -8,10 +8,10 @@ class ArticlesController < ApplicationController
  end
 
  def create
-  #render plain: params[:article].inspect
   @article = Article.new(article_params)
+  @article.user = User.first
   if @article.save
-    flash[:notice] = "Article was successfully created"
+    flash[:success] = "Article was successfully created"
     redirect_to article_path(@article)
   else
    render 'new'
@@ -29,10 +29,10 @@ class ArticlesController < ApplicationController
  def update
   @article = Article.find(params[:id])
   if @article.update(article_params)
-   flash[:notice] = "Article was updated"
+   flash[:success] = "Article was updated"
    redirect_to article_path(@article)
   else
-   flash[:notice] = "Article was not updated"
+   flash[:success] = "Article was not updated"
    render 'edit'
   end
  end
@@ -40,7 +40,7 @@ class ArticlesController < ApplicationController
  def destroy
   @article = Article.find(params[:id])
   @article.destroy
-  flash[:notice] = "Article was deleted"
+  flash[:success] = "Article was deleted"
   redirect_to articles_path
  end
 
