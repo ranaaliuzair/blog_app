fe4e288b24caf49c6703fff3cea326966dff93cb
Ali Uzair <rana.alieuzair@gmail.com>
2021-08-17 02:52:02 +0500

    all done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,8 +30,11 @@ class UsersController < ApplicationController
 
   def show
   @user = User.find(params[:id])
-   end
+  end
 
+  def index
+  @users = User.all
+  end
 
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ resources :users, except: [:new]
 resources :articles
 get 'about', to: 'pages#about'
 get 'signup', to: 'users#new'
+get 'login', to: 'sessions#new'
+post 'login', to: 'sessions#create'
+delete 'logout', to: 'sessions#destroy'
 end
